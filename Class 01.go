794184b76a011b68e829da818fc11a7fa9aa661c
dalimroy6 <dalimroy6@gmail.com>
2023-03-04 21:04:31 +0600

    first commit

---
+++ Class 01.go
@@ -0,0 +1,10 @@
+package main
+import ("fmt")
+func main () {
+// String "" ''='my name "MD. Habibur rahman" my home towen '
+// integar 0-9= int ( ata songkhape likha hoi)
+// Float 9.4 = float
+fmt.Println(5+5)
+fmt.Println(" Md. Habibur Rahman")
+fmt.Println(5.5+3.5)
+}
